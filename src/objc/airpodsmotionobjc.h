d8afe435b215cc974a27a8ce87d74a337e90aae0
liu <cr-liu@users.noreply.github.com>
2021-05-11 18:25:32 +0900

    add CMHeadphoneMotion

---
+++ src/objc/airpodsmotionobjc.h
@@ -0,0 +1,14 @@
+#ifndef AIRPODSMOTIONOBJC_H
+#define AIRPODSMOTIONOBJC_H
+
+#import <Foundation/Foundation.h>
+#import <CoreMotion/CMHeadphoneMotionManager.h>
+
+class AirpodsMotionObjc
+{
+public:
+    AirpodsMotionObjc();
+
+};
+
+#endif // AIRPODSMOTIONOBJC_H
---
+++ src/objc/airpodsmotionobjc.mm
@@ -0,0 +1,6 @@
+#include "airpodsmotionobjc.h"
+
+AirpodsMotionObjc::AirpodsMotionObjc()
+{
+
+}
